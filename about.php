7439e5828dd76b84bae4bc50709e46c371d53077
benjamin <benjamin.durin@wanadoo.fr>
2014-04-10 10:49:42 +0800

    Correct avatar hash

--- about.php
+++ about.php
@@ -5,7 +5,7 @@ if ( !defined( 'ABSPATH' ) )
 <div class="postbox" style="display: block;width:325px;float:left;margin:10px;">
 	<h3 class="hndle" style="padding:5px;"><span>About</span></h3>
 	<div class="inside">
-		<p><a href="mailto:benjamin.durin@wanadoo.fr"><img src="http://www.gravatar.com/avatar/<?php echo md5( 'benjamin'.'durin'.'@'.'wanadoo'.'.'.'fr' ); ?>" style="float:left;margin-right:10px;"/>
+		<p><a href="mailto:benjamin.durin@wanadoo.fr"><img src="http://www.gravatar.com/avatar/95915e58e3d46e52352f1c491ac5c456" style="float:left;margin-right:10px;"/>
 		<strong>Benjamin Durin</strong></a><br />
 		Multisite Invitation Codes is based on a plugin written by <a href="http://www.boiteaweb.fr">Julio Potier</a><br />
 	</div>
